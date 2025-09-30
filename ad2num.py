034d05ce04ade624453a1f6bb7332ecf0b1f0b29
Lohith G Gowda <loki@kali.local.kali>
2025-09-30 05:38:19 +0530

    added 2 new files

--- ad2num.py
+++ ad2num.py
@@ -1,8 +1,22 @@
-def addnum(num1, num2):
-    num1 = list(map(int,str(num1)))
-    num2 = list(map(int,str(num2)))
-    for i in range(len(num1)):
-        num1[i] = int(num1[i])
-        for j in range(len(num2)):
-            num2[j] = int(num2[j])
-    num1.reverse() 
+def add2num(num1, num2):
+    num1 = list(map(int, str(num1)))
+    num2 = list(map(int, str(num2)))
+    num1.reverse()
+    num2.reverse()
+    # Pad the shorter list with zeros
+    if len(num1) < len(num2):
+        num1 += [0] * (len(num2) - len(num1))
+    else:
+        num2 += [0] * (len(num1) - len(num2))
+    carry = 0
+    result = []
+    for d1, d2 in zip(num1, num2):
+        total = d1 + d2 + carry
+        result.append(total % 10)
+        carry = total // 10
+    if carry:
+        result.append(carry)
+    result.reverse()
+    return result
+
+print(add2num(123, 343))
---
+++ typecast.py
@@ -0,0 +1,14 @@
+name = "bro" 
+age = 
+height = 5.9
+student = True
+print(type(name))      # <class 'str'>
+print(type(age))       # <class 'int'>
+print(type(height))    # <class 'float'>
+print(type(student)) # <class 'bool'>
+
+
+if age = bool(age)  
+    int(input(age))
+
+print(type(age))       # <class 'int'>
